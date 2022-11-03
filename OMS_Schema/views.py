120da8116a7125b967f67d3023e9ffefae606f18
Upadhyay <rajaupadhyay@deloitte.com>
2022-11-03 17:06:41 +0530

    changes on models, calculator

---
+++ OMS_Schema/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+@api_view(['POST'])
+def calculator(request):
+    try:
+        result = None
+        if request.method == "POST":
+            operation = request.POST["operation"]
+            no1 = int(request.POST["no1"])
+            no2 = int(request.POST["no2"])
+
+            if operation == '+':
+                result = no1 + no2
+
+        return JsonResponse({"result": result})
+    except ValueError as e:
+        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
